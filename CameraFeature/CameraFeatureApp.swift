e5fbd81b1d52683cc903107186a8625aa0f15d97
AbrMac <abraarjh@gmail.com>
2026-05-27 14:35:47 +0700

    Initial Commit

---
+++ CameraFeature/CameraFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraFeatureApp.swift
+//  CameraFeature
+//
+//  Created by abr on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
